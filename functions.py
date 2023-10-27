ae43beffbb7635896972b73af9f350673a742d02
Nick Galis <48725456+nickgalis@users.noreply.github.com>
2023-10-27 12:49:49 -0400

    added "sum_eo" function

--- functions.py
+++ functions.py
@@ -10,3 +10,20 @@ if is_palindrome(phrase):
     print("'{}' is a palindrome".format(phrase))
 else:
     print("'{}' is not a palindrome".format(phrase))
+
+def sum_eo(n, t):
+    sum = -1
+    
+    if t == 'o':
+        sum = 0
+        for i in range(0, n):
+            if i % 2 == 1:
+                sum += i
+        
+    if t == 'e':
+        sum = 0
+        for i in range(0, n):
+            if i % 2 == 0:
+                sum += i
+    
+    return sum
